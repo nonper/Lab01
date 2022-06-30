cfe31f84d8d68e9bec07f0238da5caac29df38a8
biggib002 <supachok_ji@cmu.ac.th>
2022-06-30 12:32:28 +0700

    Q6.7

--- main.js
+++ main.js
@@ -11,11 +11,32 @@ const app = Vue.createApp({
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg',},
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg',}
             ],
+            cart: 0,
             sizes: ['S', 'M', 'L'],
             isOnSale: true,
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        },
+        updateStatus(){
+
+            if(this.isOnSale){
+                this.isOnSale = false;
+                console.log("false")
+            }else {
+                this.isOnSale = true;
+                console.log("true")
+            }
+
+            return this.isOnSale
         }
     }
 })
