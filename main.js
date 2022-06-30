a9cc602a3c3ec51f4945d581c1206f5588aefd7a
biggib002 <supachok_ji@cmu.ac.th>
2022-06-30 11:06:33 +0700

    Q2.5

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+const products = ""
+
+const app = Vue.createApp({
+    data() {
+        return {
+            products: "Boots",
+            description: "A boot is a type of footwear. Most boots mainly cover the foot and the ankle, while some also cover some part of the lower calf. Some boots extend up the leg, sometimes as far as the knee or even the hip."
+        }
+    }
+})
