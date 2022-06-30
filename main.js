1e692bcad901b6d94bd9c75ad9598e8c24927cb2
biggib002 <supachok_ji@cmu.ac.th>
2022-06-30 11:15:45 +0700

    Q3.6

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ const app = Vue.createApp({
     data() {
         return {
             products: "Boots",
-            description: "A boot is a type of footwear. Most boots mainly cover the foot and the ankle, while some also cover some part of the lower calf. Some boots extend up the leg, sometimes as far as the knee or even the hip."
+            description: "A boot is a type of footwear. Most boots mainly cover the foot and the ankle, while some also cover some part of the lower calf. Some boots extend up the leg, sometimes as far as the knee or even the hip.",
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.camt.cmu.ac.th/'
         }
     }
 })
