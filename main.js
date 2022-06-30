8de5ad70f678b319216afc14562493d8ae69ab45
biggib002 <supachok_ji@cmu.ac.th>
2022-06-30 11:34:33 +0700

    Q4.9

--- main.js
+++ main.js
@@ -6,7 +6,10 @@ const app = Vue.createApp({
             products: "Boots",
             description: "A boot is a type of footwear. Most boots mainly cover the foot and the ankle, while some also cover some part of the lower calf. Some boots extend up the leg, sometimes as far as the knee or even the hip.",
             image: './assets/images/socks_green.jpg',
-            url: 'https://www.camt.cmu.ac.th/'
+            url: 'https://www.camt.cmu.ac.th/',
+            inStock: true,
+            inventory: 100,
+            isOnSale: true,
         }
     }
 })
